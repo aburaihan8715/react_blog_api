9d48d9ea2baa09a090121b75ce5d8c025bd9021e
aburahan8721 <aburaihan8721@gmail.com>
2024-01-05 10:47:27 +0600

    category api done

--- routes/categories.js
+++ routes/categories.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
   const newCat = new Category(req.body);
   try {
     const savedCat = await newCat.save();
-    res.status(200).json(savedCat);
+    res.status(200).json({ message: "category created successfully!", data: savedCat });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const cats = await Category.find();
-    res.status(200).json(cats);
+    res.status(200).json({ status: "success", result: cats.length, message: "category returned!", data: cats });
   } catch (err) {
     res.status(500).json(err);
   }
