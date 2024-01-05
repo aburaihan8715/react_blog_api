080a03b29de3aace8f5e686620ca29bf1feb78cc
aburahan8721 <aburaihan8721@gmail.com>
2024-01-05 10:36:09 +0600

    users api done

--- routes/users.js
+++ routes/users.js
@@ -48,12 +48,12 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-//GET USER
+//GET A USER
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const { password, updatedAt, __v, ...others } = user._doc;
+    res.status(200).json({ message: "user returned successfully!", data: others });
   } catch (err) {
     res.status(500).json(err);
   }
